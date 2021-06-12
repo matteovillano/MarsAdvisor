1b01048e9d0ae5f88327e9e29ee54391e412f764
Bocancea1764205 <bocancea.1764205@studenti.uniroma1.it>
2021-06-12 19:35:58 +0200

    Forgot password, reset password, email sender e altre varie modifiche

--- models/User.js
+++ models/User.js
@@ -1,44 +1,53 @@
-//User.js contiene lo schema dell'utente nel database, la funzione per criptare la password prima che venga inviata al database e la funzione di login
-const mongoose = require('mongoose');                                      //Libreria con funzioni che permettono di generare schema dati per il database MongoDB
-const bcrypt = require('bcrypt');                                          //Libreria con funzioni che permettono di criptare stringhe (nel nostro caso la utilizzeremo per criptare password prima che vengano memorizzate nel database)
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const crypto = require("crypto");
 
-//Modello di dati di un user utilizzato nel database (con i vari campi presenti)
 const userSchema = new mongoose.Schema(
-    {
-        username: { type: String, required: true, unique: true},
-        email: { type: String, required: true},
-        password: { type: String, required: true},
-        date: { type: Date, default: Date.now}
-    },
-    { 
-        collection: 'users'
-    }
+  {
+    username: { type: String, required: true, unique: true },
+    email: { type: String, required: true },
+    password: { type: String, required: true },
+    date: { type: Date, default: Date.now },
+    passwordResetToken: String,
+    passwordResetExpires: Date,
+    passwordHashed: { type: Boolean, default: false },
+  },
+  {
+    collection: "users",
+  }
 );
 
-//Funzione asincrona che viene eseguita prima di salvare l'utente nel database MongoDB. Eseguo la funzione di hash della libreria bcrypt sulla password così da renderla criptata quando sarà memorizzata nel database
-userSchema.pre('save', async function(next) {
-    const salt = await bcrypt.genSalt();                                    //genera un salt (randomico), ovvero il fattore di costo di tempo (ex 10) per generare l'hash, più è alto più impiegherà tempo a essere generato
-    this.password = await bcrypt.hash(this.password, salt);                 //eseguo la funzione di hash di bcrypt fornendogli la password e il salt generato precedentemente
-    next();                                                                 //una volta eseguito l'hashing e salvato in 'password' passo alla prossima funzione nella stack
+userSchema.pre("save", async function (next) {
+  if (!this.passwordHashed) {
+    this.passwordHashed = true;
+    const salt = await bcrypt.genSalt();
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+  } else {
+    next();
+  }
 });
 
-//Funzione asincrona statica che permette di fare il login e a cui è necessario fornire le credenziali username e password (restituisce un valore booleano in caso di successo o meno)
-userSchema.statics.login = async function(username, password) {
-    const user = await this.findOne({ username });                          //prima cerca se nel database c'è lo user
-    if (user) {
-      const auth = await bcrypt.compare(password, user.password);           //funzione di bcrypt che compara le due stringhe di password, quella fornita e quella all'interno del database
-      if (auth) {
-        return user;                                                        //ritorna lo user (da notare che user è un valore booleano true o false)
-      }
-      throw Error('Password errata');                                       //errore password
+userSchema.statics.login = async function (username, password) {
+  const user = await this.findOne({ username });
+  if (user) {
+    const auth = await bcrypt.compare(password, user.password);
+    if (auth) {
+      return user;
     }
-    throw Error('Username errato');                                         //errore username
-  };
-
-const User = mongoose.model('user', userSchema);                            //Compila il modello userSchema e viene assegnato a User https://mongoosejs.com/docs/models.html (da notare che user deve essere singolare e non users!!!)
-module.exports = User;                                                      //Esporta il modello User e le sue funzioni
-
-//Riferimenti agli strumenti utilizzati
-//https://masteringjs.io/tutorials/fundamentals/class#statics qui spiega cosa sono le funzioni statiche (permettono di utilizzare "this")
-//https://www.npmjs.com/package/bcrypt BCRYPT
-//https://mongoosejs.com/docs/models.html MONGOOSE
+    throw error("errore password");
+  }
+  throw error("errore username");
+};
+userSchema.methods.createPasswordResetToken = function () {
+  const resetToken = crypto.randomBytes(32).toString("hex");
+  this.passwordResetToken = crypto
+    .createHash("sha256")
+    .update(resetToken)
+    .digest("hex");
+  console.log("reset token creato con successo");
+  this.passwordResetExpires = Date.now() + 10 * 60 * 1000;
+  return resetToken;
+};
+const User = mongoose.model("user", userSchema);
+module.exports = User;
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,12 +1,50 @@
-const { Router } = require('express');
-const { signup_get , login_get , signup_post, login_post, logout_get } = require('../controllers/authController');
+const express = require("express");
+const router = express.Router();
+const authController = require("../controllers/authController");
+const cookieParser = require("cookie-parser");
 
-const router = Router();
+router.use(express.json());
+router.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
 
-router.get('/signup', signup_get);
-router.post('/signup', signup_post);
-router.get('/login', login_get);
-router.post('/login', login_post);
-router.get('/logout', logout_get);
+router.use(cookieParser());
+
+const { requireNoAuth, checkUser } = require("../middleware/authMiddleware"); //carica la funzione che cerca di non far entrare nella pagina di login/register se l'utente è già loggato
+
+router.get("/signup", requireNoAuth, authController.signup_get);
+router.post("/signup", requireNoAuth, authController.signup_post);
+router.get("/login", requireNoAuth, authController.login_get);
+router.post("/login", requireNoAuth, authController.login_post);
+router.get("/logout", authController.logout_get);
+
+router.get(
+  "/forgotPassword",
+  requireNoAuth,
+  authController.forgot_password_get
+);
+router.post(
+  "/forgotPassword",
+  requireNoAuth,
+  authController.forgot_password_post
+);
+router.get(
+  "/resetPassword/:token",
+  requireNoAuth,
+  authController.reset_password_get
+);
+router.patch(
+  "/resetPassword/:token",
+  requireNoAuth,
+  authController.reset_password_patch
+);
+
+router.patch(
+  "/updatePassword",
+  checkUser,
+  authController.update_password_patch //ancora da implementare
+);
 
 module.exports = router;
