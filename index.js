42fe002f5e95289c2b8e3cb30998f915268c4e09
matteovillano <villano.1840807@studenti.uniroma1.it>
2021-06-21 14:14:27 +0200

    ora gli item nel DB vengono associati all'utente attraverso username

--- index.js
+++ index.js
@@ -91,7 +91,7 @@ const itemSchema=new mongoose.Schema({
     date: String,
     copyright: String,
     comment: String,
-    api_key: {
+    username: {
         type: String,
         required: true
     }
@@ -117,7 +117,8 @@ wss.on('connection',function(ws){
             return;
         }
 
-        if(!await isValidAK(ws_cmd.api_key)){
+        const username=await isValidAK(ws_cmd.api_key);
+        if(!username){
             ws.send('Invalid API_KEY, potresti non essere loggato');
             return;
         }
@@ -148,7 +149,7 @@ wss.on('connection',function(ws){
                     date: nasa_data.date,
                     copyright: nasa_data.copyright,
                     //comment: comment,
-                    api_key: api_key
+                    username: username
                 });
                 const result=await new_item.save();
                 if(result._id){
@@ -377,7 +378,7 @@ async function isValidAK(api_key){
     });
     if(user.length<1)
     return null;
-    return user;
+    return user[0].username;
 }
 
 //******************INTERFACCIA REST*********************/
@@ -386,7 +387,8 @@ async function isValidAK(api_key){
 app.get('/api/resources',async function(req,res){
     try{
         const api_key=req.query.api_key;
-        if(await isValidAK(api_key)){
+        const username=await isValidAK(api_key);
+        if(username){
             const id=req.query.id||req.body.id;
             let result;
             let sorter={};
@@ -403,7 +405,7 @@ app.get('/api/resources',async function(req,res){
             if(id){
                 result=await Item
                     .find({
-                        api_key: api_key,
+                        username: username,
                         _id: id
                     })
                     .select(selecter);
@@ -411,7 +413,7 @@ app.get('/api/resources',async function(req,res){
                 
                 result=await Item
                     .find({
-                        api_key: api_key
+                        username: username
                     })
                     .limit(parseInt(req.query.limit))
                     .sort(sorter)
@@ -432,7 +434,8 @@ app.get('/api/resources',async function(req,res){
 app.get('/api/resources/:id',async function(req,res){
     try{
         const api_key=req.query.api_key;
-        if(await isValidAK(api_key)){
+        const username=await isValidAK(api_key);
+        if(username){
             const id=req.params.id||req.query.id||req.body.id;
             let result;
             let sorter={};
@@ -450,14 +453,14 @@ app.get('/api/resources/:id',async function(req,res){
                 result=await Item
                     .findById(id)
                     .find({
-                        api_key: api_key
+                        username: username
                     })
                     .select(selecter);
             }else{
                 
                 result=await Item
                     .find({
-                        api_key: api_key
+                        username: username
                     })
                     .limit(parseInt(req.query.limit))
                     .sort(sorter)
@@ -478,7 +481,8 @@ app.get('/api/resources/:id',async function(req,res){
 //***************POST**********************/
 app.post('/api/resources/apod',async function(req,res){
     const api_key=req.query.api_key||req.body.api_key;
-    if(await isValidAK(api_key)){
+    const username=await isValidAK(api_key);
+    if(username){
         try{
             const date=req.body.date||(new Date).toISOString().slice(0,10);
             const comment=req.body.comment;
@@ -491,7 +495,6 @@ app.post('/api/resources/apod',async function(req,res){
             
             const nasa_data=nasa_res.data;
             //console.log(nasa_data);
-            
             const new_item=new Item({
                 title: nasa_data.title,
                 media_type: nasa_data.media_type,
@@ -501,7 +504,7 @@ app.post('/api/resources/apod',async function(req,res){
                 date: nasa_data.date,
                 copyright: nasa_data.copyright,
                 comment: comment,
-                api_key: api_key
+                username: username
             });
             const result=await new_item.save();
             //console.log('oleeee');
@@ -521,11 +524,12 @@ app.post('/api/resources/apod',async function(req,res){
 //*********************PUT***************************/
 app.put('/api/resources',async function(req,res){
     const api_key=req.query.api_key||req.body.api_key;
-    if(await isValidAK(api_key)){
+    const username=await isValidAK(api_key);
+    if(username){
 
         const result=await Item.updateOne({
             _id: req.body.id,
-            api_key: api_key
+            username: username
         },{
             comment: req.body.comment
         });
@@ -538,11 +542,12 @@ app.put('/api/resources',async function(req,res){
 
  app.put('/api/resources/one',async function(req,res){
     const api_key=req.query.api_key||req.body.api_key;
-    if(await isValidAK(api_key)){
+    const username=await isValidAK(api_key);
+    if(username){
         const new_comment=req.body.new_comment;
         let filter=req.body;
         filter['new_comment']=undefined;
-        filter['api_key']=api_key;
+        filter['username']=username;
         filter['comment']=filter['old_comment'];
         filter['old_comment']=undefined;
 
@@ -558,11 +563,12 @@ app.put('/api/resources',async function(req,res){
 
  app.put('/api/resources/many',async function(req,res){
     const api_key=req.query.api_key||req.body.api_key;
-    if(await isValidAK(api_key)){
+    const username=await isValidAK(api_key);
+    if(username){
         const new_comment=req.body.new_comment;
         let filter=req.body;
         filter['new_comment']=undefined;
-        filter['api_key']=api_key;
+        filter['username']=username;
         filter['comment']=filter['old_comment'];
         filter['old_comment']=undefined;
 
@@ -578,11 +584,12 @@ app.put('/api/resources',async function(req,res){
 //*********************DELETE***************************/
 app.delete('/api/resources', async function(req,res){
     const api_key=req.query.api_key||req.body.api_key;
-    if(await isValidAK(api_key)){
+    const username=await isValidAK(api_key);
+    if(username){
         if(req.body.id){
             const result= await Item.deleteOne({
                 _id: req.body.id,
-                api_key: api_key
+                username: username
             });
             res.send(result);
         }else{
@@ -596,9 +603,10 @@ app.delete('/api/resources', async function(req,res){
 
  app.delete('/api/resources/one', async function(req,res){
     const api_key=req.query.api_key||req.body.api_key;
-    if(await isValidAK(api_key)){
+    const username=await isValidAK(api_key);
+    if(username){
         let filter=req.body;
-        filter['api_key']=api_key;
+        filter['username']=username;
         //console.log(filter);
         const result=await Item.deleteOne(filter);
         res.send(result);
@@ -610,9 +618,10 @@ app.delete('/api/resources', async function(req,res){
 
  app.delete('/api/resources/many', async function(req,res){
     const api_key=req.query.api_key||req.body.api_key;
-    if(await isValidAK(api_key)){
+    const username=await isValidAK(api_key);
+    if(username){
         let filter=req.body;
-        filter['api_key']=api_key;
+        filter['username']=username;
         //console.log(filter);
         const result=await Item.deleteMany(filter);
         res.send(result);
